2305a383fba8ca276fdf79560365cea19b27f712
SmitaGit19 <smitablr19@gmail.com>
2020-01-30 23:42:37 +0530

    first unique character in string

--- src/strings/FirstUniqueCharacter.java
+++ src/strings/FirstUniqueCharacter.java
@@ -1,31 +1,42 @@
 package strings;
 
-import java.util.LinkedList;
+
 
 public class FirstUniqueCharacter {
 
 	public static void main(String[] args) {
 
-		System.out.println(" The first unique character in the string is : " + firstUniqChar("leetcode"));
+		System.out.println(" The first unique character in the string is : " + firstUniqChar("llooveleetcodetdcv"));
 	}
 
 	private static int firstUniqChar(String s) {
-		if (s == null || s.length() == 0)
-			return -1;
-		LinkedList<Character> listChar = new LinkedList<>();
-		for (char c : s.toCharArray()) {
-			if (listChar.contains(c)) {
-				listChar.remove(listChar.indexOf(c));
-			} else {
-				listChar.add(c);
-			}
-		}
-
-		return listChar.peek() == null ? -1 : s.charAt(listChar.indexOf(listChar.peek()));
+		return 0;
 	}
+	
+	
+	//-----------------PASSED BUT NOT EFFICIENT-------------
+	// works fine with complexity O(n) but its too slow takes 124 ms to execute and takes lot of space.
+//	private static int firstUniqChar(String s) {
+//		if (s == null || s.length() == 0)
+//			return -1;
+//		LinkedList<Character> listChar = new LinkedList<>();
+//		List<Character> deletedCharList = new ArrayList<Character>();
+//		
+//		for (char c : s.toCharArray()) {
+//			if (listChar.contains(c)) {
+//				listChar.remove(listChar.indexOf(c));
+//				deletedCharList.add(c);
+//			} else if(!deletedCharList.contains(c)){
+//				listChar.add(c);
+//			}
+//		}
+//		System.out.println("the peek is "+ listChar.peek());
+//		return listChar.peek() == null ? -1 : s.indexOf(listChar.peek());
+//	}
 
+	//--------------FAILED---------
 	// First fialed attemp the PriorityQueue is not same as the normal Queue.
-	// private static int firstUniqChar(String s) {
+	// private static int firstUniqChar(String s) { // 
 	// if(s == null || s.length() == 0)
 	// return -1;
 	// PriorityQueue<Character> queue = new PriorityQueue<Character>();
